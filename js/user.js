af6964e89d2d97e5541427fa69b8476324e11e8c
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-10-14 09:18:50 -0700

    refactor user constructor to hold information about user and prototype to request user object from github api.

--- js/user.js
+++ js/user.js
@@ -1,20 +1,17 @@
 var apiKey = require('./../.env').apiKey;
 
 User = function() {
-}
+  this.name;
+  this.photo;
+  this.followerCount;
+  this.repos = [];
+};
 
-User.prototype.getRepos = function(username, displayFunction){
-  var repoNames = [];
-  var repoDescriptions = [];
-  var repoDates = [];
-  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
-    console.log(moment(response[0].created_at).format('L'));
-    for (var i = 0; i < response.length; i++) {
-      repoNames.push(response[i].name);
-      repoDescriptions.push(response[i].description);
-      repoDates.push(moment(response[i].created_at).format('L'));
-    }
-    displayFunction(repoNames, repoDescriptions, repoDates);
+User.prototype.setUser = function(username){
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+    console.log(response);
+
+    //displayFunction(repoNames, repoDescriptions, repoDates);
   }).fail(function(error){
     $("#search-results").text("User: " + error.responseJSON.message + ". Please check the username and try searching again.");
   });
