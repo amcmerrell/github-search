7bcb403e5c0d8bca7193e8a757d41cc97a04efc6
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-10-14 07:51:50 -0700

    add displayRepos function that currently only displays name of first repo.

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,10 +1,14 @@
 var User = require('./../js/user.js').userModule;
 
+var displayRepos = function(searchResults) {
+  $('#search-results').text('First repo name: ' + searchResults);
+}
+
 var currentUser = new User();
 $(document).ready(function() {
   $("#user-search").submit(function(event) {
     event.preventDefault();
     var username = $("#username").val();
-    currentUser.getRepos(username);
+    currentUser.getRepos(username, displayRepos);
   })
 });
