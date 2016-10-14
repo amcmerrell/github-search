e9f4ad533c6b196370c62c39dd84dab98028828c
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-10-14 11:31:08 -0700

    add jQuery to run getRepos when find-repos button is clicked.

--- js/repo-interface.js
+++ js/repo-interface.js
@@ -1,8 +1,18 @@
+var Repo = require('./../js/repo.js').repoModule;
+
 var displayRepos = function(repoName, repoDescription, repoDate) {
-  console.log(repoName);
   for (var i = 0; i < repoName.length; i++) {
-    $('#search-results').append('<h3>Repo name: ' + repoName[i] + '</h3>');
-    $('#search-results').append('<p>description: ' + repoDescription[i] + '</p>');
-    $('#search-results').append('<p>created: ' + repoDate[i] + '</p>');
+    $("#search-results").append("<h3>Repo name: " + repoName[i] + "</h3>");
+    $("#search-results").append("<p>description: " + repoDescription[i] + "</p>");
+    $("#search-results").append("<p>created: " + repoDate[i] + "</p>");
   }
 };
+
+var currentRepo = new Repo();
+$(document).ready(function() {
+  $("#find-repos").click(function() {
+    debugger;
+    var username = $("#username").val();
+    currentRepo.getRepos(username, displayRepos);
+  });
+});
