e51981c0eefd464ce0419f24971531df267f76f6
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-10-14 12:02:27 -0700

    adjust headers in appended content.

--- js/repo-interface.js
+++ js/repo-interface.js
@@ -1,17 +1,22 @@
 var Repo = require('./../js/repo.js').repoModule;
 
 var displayRepos = function(repoName, repoDescription, repoDate) {
+  $("#search-results").append("<h3>Repositories</h3>");
   for (var i = 0; i < repoName.length; i++) {
-    $("#search-results").append("<h3>Repo name: " + repoName[i] + "</h3>");
-    $("#search-results").append("<p>description: " + repoDescription[i] + "</p>");
-    $("#search-results").append("<p>created: " + repoDate[i] + "</p>");
+    $("#search-results").append("<h4>" + repoName[i] + "</h4>");
+    if (repoDescription[i]) {
+      $("#search-results").append("<p>Description: " + repoDescription[i] + "</p>");
+    } else {
+      $("#search-results").append("<p>Description: No description.</p>");
+    }
+
+    $("#search-results").append("<p>Created: " + repoDate[i] + "</p><hr>");
   }
 };
 
 var currentRepo = new Repo();
 $(document).ready(function() {
   $("#find-repos").click(function() {
-    debugger;
     var username = $("#username").val();
     currentRepo.getRepos(username, displayRepos);
   });
