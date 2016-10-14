1eaa9b381e36cb696c7bdbd13ab318be737a6a41
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-10-13 21:16:37 -0700

    add API sample code to user.js and basic submit function to user-interface.js.

---
+++ js/user-interface.js
@@ -0,0 +1,10 @@
+var User = require('./../js/user.js').userModule;
+
+var currentUser = new User();
+$(document).ready(function() {
+  $("#user-search").submit(function(event) {
+    event.preventDefault();
+    var username = $("#username").val();
+    currentUser.getRepos();
+  })
+});
