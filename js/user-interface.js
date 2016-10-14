ec2a303fe289f82dfd903e726252361453b437e0
Andrew Merrell <andrew.m.merrell@gmail.com>
2016-10-14 08:50:21 -0700

    include repo date information in getRepos function and pass data to displayRepos function.

--- js/user-interface.js
+++ js/user-interface.js
@@ -1,10 +1,11 @@
 var User = require('./../js/user.js').userModule;
 
-var displayRepos = function(repoName, repoDescription) {
+var displayRepos = function(repoName, repoDescription, repoDate) {
   console.log(repoName);
   for (var i = 0; i < repoName.length; i++) {
     $('#search-results').append('<h3>Repo name: ' + repoName[i] + '</h3>');
     $('#search-results').append('<p>description: ' + repoDescription[i] + '</p>');
+    $('#search-results').append('<p>created: ' + repoDate[i] + '</p>');
   }
 }
 
--- js/user.js
+++ js/user.js
@@ -6,12 +6,15 @@ User = function() {
 User.prototype.getRepos = function(username, displayFunction){
   var repoNames = [];
   var repoDescriptions = [];
+  var repoDates = [];
   $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+    console.log(moment(response[0].created_at).format('L'));
     for (var i = 0; i < response.length; i++) {
       repoNames.push(response[i].name);
       repoDescriptions.push(response[i].description);
+      repoDates.push(moment(response[i].created_at).format('L'));
     }
-    displayFunction(repoNames, repoDescriptions);
+    displayFunction(repoNames, repoDescriptions, repoDates);
   }).fail(function(error){
     displayFunction(error.responseJSON.message);
   });
